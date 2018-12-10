c82db747cdcada3aa42bb474b659c3973c390801
KMarek <marek.z.krzysztof@gmail.com>
2018-12-10 10:31:52 +0100

    First SmartAI implementation

--- src/main/java/TTT/SmartAI.java
+++ src/main/java/TTT/SmartAI.java
@@ -2,12 +2,155 @@ package TTT;
 
 public class SmartAI {
     public static int[] findWinningMove() {
-        return new int[] {-10, -10};
+        int checker = 0;
+        int x = -1;
+        int y = -1;
+        for(int i=0; i<3; i++)
+        {
+            checker = 0;
+            for(int j=0; j<3; j++)
+            {
+                if(GameLogic.board[i][j]=='O')
+                    checker++;
+                if(GameLogic.board[i][j]=='X')
+                    checker--;
+                if(GameLogic.board[i][j]==' '){
+                    x=i;
+                    y=j;
+                }
+            }
+            if(checker == 2)
+                return new int[] {x,y};
+
+        }
+        for(int i=0; i<3; i++)
+        {
+            checker = 0;
+            for(int j=0; j<3; j++)
+            {
+                if(GameLogic.board[j][i]=='O')
+                    checker++;
+                if(GameLogic.board[j][i]=='X')
+                    checker--;
+                if(GameLogic.board[j][i]==' '){
+                    x=j;
+                    y=i;
+                }
+            }
+            if(checker == 2)
+                return new int[] {x,y};
+
+        }
+        checker=0;
+        for(int i=0; i<3; i++)
+        {
+            if(GameLogic.board[i][i]=='O')
+                checker++;
+            if(GameLogic.board[i][i]=='X')
+                checker--;
+            if(GameLogic.board[i][i]==' '){
+                x=i;
+                y=i;
+            }
+        }
+        if(checker == 2)
+            return new int[] {x,y};
+        checker=0;
+        for(int i=0; i<3; i++)
+        {
+            if(GameLogic.board[i][2-i]=='O')
+                checker++;
+            if(GameLogic.board[i][2-i]=='X')
+                checker--;
+            if(GameLogic.board[i][2-i]==' '){
+                x=i;
+                y=2-i;
+            }
+        }
+        if(checker == 2)
+            return new int[] {x,y};
+
+        return new int[] {-1, -1};
     }
     public static int[] findPreventingMove() {
-        return new int[] {-10, -10};
+        int checker = 0;
+        int x = -1;
+        int y = -1;
+        for(int i=0; i<3; i++)
+        {
+            checker = 0;
+            for(int j=0; j<3; j++)
+            {
+                if(GameLogic.board[i][j]=='O')
+                    checker--;
+                if(GameLogic.board[i][j]=='X')
+                    checker++;
+                if(GameLogic.board[i][j]==' '){
+                    x=i;
+                    y=j;
+                }
+            }
+            if(checker == 2)
+                return new int[] {x,y};
+
+        }
+        for(int i=0; i<3; i++)
+        {
+            checker = 0;
+            for(int j=0; j<3; j++)
+            {
+                if(GameLogic.board[j][i]=='O')
+                    checker--;
+                if(GameLogic.board[j][i]=='X')
+                    checker++;
+                if(GameLogic.board[j][i]==' '){
+                    x=j;
+                    y=i;
+                }
+            }
+            if(checker == 2)
+                return new int[] {x,y};
+
+        }
+        checker=0;
+        for(int i=0; i<3; i++)
+        {
+            if(GameLogic.board[i][i]=='O')
+                checker--;
+            if(GameLogic.board[i][i]=='X')
+                checker++;
+            if(GameLogic.board[i][i]==' '){
+                x=i;
+                y=i;
+            }
+        }
+        if(checker == 2)
+            return new int[] {x,y};
+        checker=0;
+        for(int i=0; i<3; i++)
+        {
+            if(GameLogic.board[i][2-i]=='O')
+                checker--;
+            if(GameLogic.board[i][2-i]=='X')
+                checker++;
+            if(GameLogic.board[i][2-i]==' '){
+                x=i;
+                y=2-i;
+            }
+        }
+        if(checker == 2)
+            return new int[] {x,y};
+
+        return new int[] {-1, -1};
     }
     public static int[] findGoodMove() {
+        int[] x = {1,0,0,2,2,0,1,1,2};
+        int[] y = {1,0,2,0,2,1,0,2,1};
+        for(int i=0; i<9; i++)
+        {
+            if(!GameLogic.isOccupied(x[i],y[i]))
+                return new int[] {x[i],y[i]};
+        }
         return new int[] {-10, -10};
     }
 }
